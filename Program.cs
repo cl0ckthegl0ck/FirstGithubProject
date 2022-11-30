626037da762ddf9cc37a49ba02c0d038d25eb37d
1Tp <1Tp@207-10>
2022-11-30 12:18:36 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstGIthubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
